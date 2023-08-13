71e0990f2c23b0d102d969554e8a227830c27171
Pavel <paveldu2111@gmail.com>
2023-08-13 23:46:45 +0300

    Initial Commit

---
+++ SwiftUIPetProject/SwiftUIPetProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPetProjectApp.swift
+//  SwiftUIPetProject
+//
+//  Created by Павел Дю on 13.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPetProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
