7c86192f3a2b47f35547294b3c9ddd5c9f5490fb
Pavel <paveldu2111@gmail.com>
2023-08-14 13:01:43 +0300

    Setting NavigationBar
     Add toolbar

--- SwiftUIPetProject/ContentView.swift
+++ SwiftUIPetProject/ContentView.swift
@@ -8,16 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+//    init() {
+//        UITabBar.appearance().backgroundColor = .gray
+//    }
+    @State private var selectedView = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        TabView(selection: $selectedView) {
+            //  Вкладка 1
+            NavigationView {
+                VStack {
+                    
+                } .toolbar {
+                    NavigationBar()
+                }
+             
+            }
+            
+            .tabItem {
+                Image(systemName: "1.circle")
+                Text("Вкладка 1")
+            } .tag(1)
+            
+            
+            // Вкладка 2
+            NavigationView {
+                VStack {
+                    Text("Содержимое второй вкладки")
+                }
+                .navigationBarTitle("Вкладка 2", displayMode: .inline)
+            }
+            .tabItem {
+                Image(systemName: "2.circle")
+                Text("Вкладка 2")
+            }
+            
+            // Вкладка 3
+            NavigationView {
+                VStack {
+                    Text("Содержимое третьей вкладки")
+                }
+                .navigationBarTitle("Вкладка 3", displayMode: .inline)
+            }
+            .tabItem {
+                Image(systemName: "3.circle")
+                Text("Вкладка 3")
+            }
+            
+            // Вкладка 4
+            NavigationView {
+                VStack {
+                    Text("Содержимое третьей вкладки")
+                }
+                .navigationBarTitle("Вкладка 4", displayMode: .inline)
+            }
+            .tabItem {
+                Image(systemName: "4.circle")
+                Text("Вкладка 4")
+            }
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
